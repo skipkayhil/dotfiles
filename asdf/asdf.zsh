8b4275e6622556109a37444291174f1d19cfd026
SkipKayhil <skipkayhil@gmail.com>
2019-03-11 22:31:18 -0400

    move tool-versions into .config

--- asdf/asdf.zsh
+++ asdf/asdf.zsh
@@ -1,4 +1,5 @@
 export ASDF_CONFIG_FILE="$DOT_CONFIG_HOME/asdf/asdfrc"
 export ASDF_DATA_DIR="$DOT_CACHE_HOME/asdf"
+export ASDF_DEFAULT_TOOL_VERSIONS_FILENAME="$DOT_CONFIG_HOME/asdf/tool-versions"
 
 . $DOT_CACHE_HOME/asdf/asdf.sh
