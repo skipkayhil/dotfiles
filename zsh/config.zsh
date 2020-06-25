ffdf9d6573cc4f41b71deb1fb77d3836ff8bcde3
Hartley McGuire <skipkayhil@gmail.com>
2020-06-24 22:08:38 -0400

    add tm function, update tmux config
    - add tm function for creating/switching between sessions with fzf
    - enable autoloading for zsh/functions/ since we added tm
    - add tmux configuration to open windows in same dir (since new goal is
    to use a session per project)

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -10,7 +10,7 @@ fi
 # updated fpath for future functions, autoload errors if empty so its commented
 fpath=($DOTFILES/zsh/functions $fpath)
 
-# autoload -U $DOTFILES/zsh/functions/*(:t)
+autoload -U $DOTFILES/zsh/functions/*(:t)
 
 HISTFILE="$XDG_DATA_HOME/zsh/history"
 HISTSIZE=10000
