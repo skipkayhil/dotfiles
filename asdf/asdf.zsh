87149231cd9ca9f7edbc62a4efdcf14a5c551cf6
Hartley McGuire <skipkayhil@gmail.com>
2020-08-02 20:31:31 -0400

    replace DOT_* vars with XDG_*

--- asdf/asdf.zsh
+++ asdf/asdf.zsh
@@ -1,6 +1,6 @@
-export ASDF_CONFIG_FILE="$DOT_CONFIG_HOME/asdf/asdfrc"
-export ASDF_DATA_DIR="$DOT_CACHE_HOME/asdf"
-export ASDF_DEFAULT_TOOL_VERSIONS_FILENAME="$DOT_CONFIG_HOME/asdf/tool-versions"
+export ASDF_CONFIG_FILE="$XDG_CONFIG_HOME/asdf/asdfrc"
+export ASDF_DATA_DIR="$XDG_CACHE_HOME/asdf"
+export ASDF_DEFAULT_TOOL_VERSIONS_FILENAME="$XDG_CONFIG_HOME/asdf/tool-versions"
 
-. $DOT_CACHE_HOME/asdf/asdf.sh
+. $XDG_CACHE_HOME/asdf/asdf.sh
 fpath=(${ASDF_DIR}/completions $fpath)
