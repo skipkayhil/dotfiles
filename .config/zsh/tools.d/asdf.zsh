04fed784e7fa6c90e7e45d272e142f0f1b7796c2
Hartley McGuire <skipkayhil@gmail.com>
2022-05-09 22:52:19 -0400

    only load asdf configuration if installed

--- .config/zsh/tools.d/asdf.zsh
+++ .config/zsh/tools.d/asdf.zsh
@@ -1,3 +1,7 @@
+if [[ ! -d "$XDG_CACHE_HOME/asdf" ]]; then
+  return
+fi
+
 export ASDF_CONFIG_FILE="$XDG_CONFIG_HOME/asdf/asdfrc"
 export ASDF_DATA_DIR="$XDG_CACHE_HOME/asdf"
 export ASDF_DEFAULT_TOOL_VERSIONS_FILENAME="$XDG_CONFIG_HOME/asdf/tool-versions"
