dde51de8ead1cbe75c434f679075c3596698f30b
Hartley McGuire <skipkayhil@gmail.com>
2023-05-05 16:52:18 -0400

    Fix "Missing user config file" in neovim
    Running :checkhealth would show a warning that init.vim is missing. To
    fix this, the single/double quotes have been inverted so that the value
    of $XDG_CONFIG_HOME gets put into $VIMINIT instead of the variable.

--- .config/zsh/tools.d/vim.zsh
+++ .config/zsh/tools.d/vim.zsh
@@ -1,1 +1,1 @@
-export VIMINIT='let $MYVIMRC = !has("nvim") ? "$XDG_CONFIG_HOME/vim/vimrc" : "$XDG_CONFIG_HOME/nvim/init.vim" | source $MYVIMRC'
+export VIMINIT="let \$MYVIMRC = !has('nvim') ? '$XDG_CONFIG_HOME/vim/vimrc' : '$XDG_CONFIG_HOME/nvim/init.vim' | source \$MYVIMRC"
