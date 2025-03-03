a6e103dd7ae28344313ba2d7037af89cd58b6651
Hartley McGuire <skipkayhil@gmail.com>
2025-03-03 10:59:59 -0500

    Make prompt character purple again
    This was accidentally changed during the p10k update

--- .config/zsh/p10k.zsh
+++ .config/zsh/p10k.zsh
@@ -192,7 +192,7 @@
 
   ################################[ prompt_char: prompt symbol ]################################
   # Green prompt symbol if the last command succeeded.
-  typeset -g POWERLEVEL9K_PROMPT_CHAR_OK_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=2
+  typeset -g POWERLEVEL9K_PROMPT_CHAR_OK_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=5
   # Red prompt symbol if the last command failed.
   typeset -g POWERLEVEL9K_PROMPT_CHAR_ERROR_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=1
   # Default prompt symbol.
