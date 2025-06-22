62dfc83c40088ed4eeeffe089c4c41fefadd0a2a
Hartley McGuire <skipkayhil@gmail.com>
2025-06-21 23:53:56 -0400

    Use harmonic-evil for fzf
    Generated from Vim using the guide in the fzf [ADVANCED][1] doc
    
    [1]: https://github.com/junegunn/fzf/blob/3a21116307d31a35d499776ed8604cc2818f4378/ADVANCED.md#generating-fzf-color-theme-from-vim-color-schemes

--- .config/zsh/tools.d/fzf.zsh
+++ .config/zsh/tools.d/fzf.zsh
@@ -16,3 +16,4 @@ source "$DOTFILES/opt/fzf-tab/fzf-tab.zsh"
 
 export FZF_DEFAULT_COMMAND="fd --type file --follow --hidden --exclude .git"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_DEFAULT_OPTS="--color=bg+:#474a58,bg:#2b2e3b,border:#cacedf,spinner:#e3cd94,hl:#808393,fg:#cacedf,header:#808393,info:#e3cd94,pointer:#fc8083,marker:#afb5ff,fg+:#cacedf,preview-bg:#2b2e3b,prompt:#afb5ff,hl+:#fc8083"
