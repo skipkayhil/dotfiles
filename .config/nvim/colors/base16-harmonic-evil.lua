4d8b7aac24d45a4634d14b726f13399fc7bf8e09
Hartley McGuire <skipkayhil@gmail.com>
2025-10-28 18:49:52 -0400

    Use lua for nvim harmonic-evil colors

---
+++ .config/nvim/colors/base16-harmonic-evil.lua
@@ -0,0 +1,9 @@
+vim.cmd("hi clear")
+vim.g.colors_name = 'base16-harmonic-evil'
+
+require('base16-colorscheme').setup({
+    base00 = '#2b2e3b', base01 = '#474a58', base02 = '#666978', base03 = '#808393',
+    base04 = '#b5b9ca', base05 = '#cacedf', base06 = '#dfe3f4', base07 = '#f0f3ff',
+    base08 = '#fc8083', base09 = '#fba581', base0A = '#e3cd94', base0B = '#a9c470',
+    base0C = '#92ddce', base0D = '#9fd5fc', base0E = '#afb5ff', base0F = '#f0a1d6'
+})
